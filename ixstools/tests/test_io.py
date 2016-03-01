478e39d6f0bb437c6df5d27b3bfe41144660229d
Eric Dill <edill@bnl.gov>
2016-03-01 10:50:12 -0500

    Remove plot test; add scan data test

--- ixstools/tests/test_io.py
+++ ixstools/tests/test_io.py
@@ -21,16 +21,6 @@ def test_specfile_header_parsing(specfile_object):
     assert (len(sf.parsed_header['detector_spec_names']) ==
             len(sf.parsed_header['detector_human_names']))
 
-
-def test_specfile_plotting(specfile_object):
-    import matplotlib
-    matplotlib.use('agg')
-    fig, ax = matplotlib.pyplot.subplots()
-    for scan in specfile_object:
-        # select a random column from within the spec file, but
-        # because the seed is set, it will be the *same* random column
-        # each time...
-        col_name = random.choice(scan.col_names)
-        arts = scan.plot(ax, column_names=[col_name])
-        assert len(arts[col_name].get_xdata()) > 0
-
+    for scan in sf:
+        assert (len(sf.parsed_header['motor_spec_names']) ==
+                len(scan.motor_values))
